1ab5fb5d3496d11e42b1af4b31f3ddab39bfd66f
Your Name <abdelwahab.jomla@gmaile.com>
2025-01-29 08:11:56 +0100

    Save

--- app/src/main/java/Z_MasterOfApps/Kotlin/ViewModel/ViewModelInitApp.kt
+++ app/src/main/java/Z_MasterOfApps/Kotlin/ViewModel/ViewModelInitApp.kt
@@ -1,8 +1,8 @@
 package Z_MasterOfApps.Kotlin.ViewModel
 
+import Z_MasterOfApps.Kotlin.Model._ModelAppsFather
 import Z_MasterOfApps.Z.Android.Packages._1.GerantAfficheurGrossistCommend.App.NH_2.id1_GerantDefinirePosition.ViewModel.Extension.ViewModelExtension_App1_F1
 import Z_MasterOfApps.Z.Android.Packages._1.GerantAfficheurGrossistCommend.App.NH_3.id2_TravaillieurListProduitAchercheChezLeGrossist.ViewModel.Extension.ViewModelExtension_App1_F2
-import Z_MasterOfApps.Kotlin.Model._ModelAppsFather
 import Z_MasterOfApps.Z_AppsFather.Kotlin._1.Model.ParamatersAppsModel
 import Z_MasterOfApps.Z_AppsFather.Kotlin._3.Init.A_LoadFireBase.LoadFromFirebaseProduits
 import Z_MasterOfApps.Z_AppsFather.Kotlin._3.Init.CreeDepuitAncienDataBases
@@ -15,15 +15,12 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.osmdroid.views.MapView
 
 @SuppressLint("SuspiciousIndentation")
 class ViewModelInitApp : ViewModel() {
     var _paramatersAppsViewModelModel by mutableStateOf(ParamatersAppsModel())
     var _modelAppsFather by mutableStateOf(_ModelAppsFather())
-    var mapViewVM by mutableStateOf<MapView?>(null)
-        private set
-   
+
     val modelAppsFather: _ModelAppsFather get() = _modelAppsFather
     val produitsMainDataBase = _modelAppsFather.produitsMainDataBase
 
