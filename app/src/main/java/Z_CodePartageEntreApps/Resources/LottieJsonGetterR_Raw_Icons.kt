c2ca0b3f2f7044b54bb57b5a4761c09db367bb45
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-03-13 14:45:19 +0100

    Save

--- app/src/main/java/Z_CodePartageEntreApps/Resources/LottieJsonGetterR_Raw_Icons.kt
+++ app/src/main/java/Z_CodePartageEntreApps/Resources/LottieJsonGetterR_Raw_Icons.kt
@@ -4,7 +4,7 @@ import com.example.clientjetpack.R
 
 enum class LottieJsonGetterR_Raw_Icons(val resourceId: Int) {
     reacticonanimatedjsonurl(R.raw.reacticonanimatedjsonurl),
-    afficheFenetre(R.raw.stats_lottie_json) ,
-    alimentation(R.raw.alimentation)   ,
+    afficheFenetre(R.raw.stats_lottie_json),
+    alimentation(R.raw.alimentation),
     atay(R.raw.atay)
 }
