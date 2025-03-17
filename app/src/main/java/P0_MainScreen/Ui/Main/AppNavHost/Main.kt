750a96cf5ec8c57ceaa3a6c3c02a900cc39ff84f
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-03-17 09:47:41 +0100

    Version >> 1.0

--- app/src/main/java/P0_MainScreen/Ui/Main/AppNavHost/Main.kt
+++ app/src/main/java/P0_MainScreen/Ui/Main/AppNavHost/Main.kt
@@ -212,7 +212,7 @@ fun AppNavHost(
 
                 composable(Screen.TravailleTempRecorder.route) {
                     Box(modifier = Modifier.fillMaxSize()) {
-                        MainScreen_Windows(fabsVisibility=viewModelInitApp._paramatersAppsViewModelModel.fabsVisibility)
+                        MainScreen_Windows(fabsVisibility = viewModelInitApp._paramatersAppsViewModelModel.fabsVisibility)
                     }
                 }
             }
