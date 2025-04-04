143e6adace1e9c6f9c3a7a6c6ab8fb1416d8e2fb
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-04-04 19:17:25 +0100

    before RElode

--- app/src/main/java/Z_CodePartageEntreApps/Apps/Manager/Module/A/Koin/AppManagerModules.kt
+++ app/src/main/java/Z_CodePartageEntreApps/Apps/Manager/Module/A/Koin/AppManagerModules.kt
@@ -1,6 +1,5 @@
 package Z_CodePartageEntreApps.Apps.Manager.Module.A.Koin
 
-import W.Fragments.A.PanierFinaleDAchat.APP.ViewModel.ViewModelFragment_APP2_FragID_1
 import Z_CodePartageEntreApps.Proto.B.Sectiones.Fragment.B.DataBaseManager.App.A.CategoryReorderAndSelection.Package.ViewModel.ViewModel_A4FragID1
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -11,5 +10,4 @@ val appManagerModules = module {
 
 
     viewModel { ViewModel_A4FragID1(get(), get()) }
-    viewModel { ViewModelFragment_APP2_FragID_1(get(),get(),get(),get()) }
 }
