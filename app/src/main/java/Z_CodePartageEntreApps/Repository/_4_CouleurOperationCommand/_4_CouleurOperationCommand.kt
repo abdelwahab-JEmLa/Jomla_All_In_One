39cac50f6a1a5ba14693c37735308ed89b8be9da
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-04-12 13:24:44 +0100

    regle inseration

--- app/src/main/java/Z_CodePartageEntreApps/Repository/_4_CouleurOperationCommand/_4_CouleurOperationCommand.kt
+++ app/src/main/java/Z_CodePartageEntreApps/Repository/_4_CouleurOperationCommand/_4_CouleurOperationCommand.kt
@@ -1,41 +1,20 @@
 package Z_CodePartageEntreApps.Repository._4_CouleurOperationCommand
 
-import androidx.compose.ui.graphics.Color
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class _4_CouleurOperationCommand(
     @PrimaryKey(autoGenerate = true)
-    var vid: Long = 0,
+
+    // Section Related Parents Foreign Key IDs
+    var couleurIndex_ParentVID: Long = 0L,
+    var produitVID_ParentKey: Long? =null,
 
     // Section InfosDeBase
-    var nom: String = "",
-    var emballageCartone: Boolean = false,
+    var totaleQuantity: Int = 0,
 
-    // Section Etates Mutable
-    var itsTempProduit: Boolean = false,
-    var besoinToBeUpdated: Boolean = false,
-    var non_Trouve: Boolean = false,
-    var isVisible: Boolean = true,
-    var imageGlidReloadTigger: Int = 0,
-    var prePourCameraCapture: Boolean = true,
-    var diponibilityEtate: Boolean = true,
-    var probablementNonDispo: Boolean = true,
-    var enumVarNonDispoPourClients: NON_DISPO_POUR_CLIENTS = NON_DISPO_POUR_CLIENTS.DISPONIBLE_POUR_TOUT,
 
-    // Section sonCategory
-    var parentCategoryId: Long = 0L,
-    var indexInParentCategorie: Int = 0,
+    ) {
 
-    // Section InfosCoutes
-    var monPrixAchat: Double = 0.0,
-    var monPrixVent: Double = 0.0,
-) {
-    enum class NON_DISPO_POUR_CLIENTS(val color: Color) {
-        DISPONIBLE_POUR_TOUT(Color(0xFF4CAF50)),
-        TOUT(Color(0xFFF44336)),
-        NEVEAU(Color(0xFFFF9800)),
-        DEFINIE(Color(0xFF2196F3));
-    }
 }
