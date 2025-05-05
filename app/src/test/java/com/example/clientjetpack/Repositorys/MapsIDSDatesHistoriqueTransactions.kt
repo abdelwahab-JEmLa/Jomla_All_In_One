13e47f64921c7c5c67b949f1e4f7432ab9ef5463
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-05 19:07:00 +0100

    .

--- app/src/test/java/com/example/clientjetpack/Repositorys/MapsIDSDatesHistoriqueTransactions.kt
+++ app/src/test/java/com/example/clientjetpack/Repositorys/MapsIDSDatesHistoriqueTransactions.kt
@@ -3,8 +3,7 @@ package com.example.clientjetpack.Repositorys
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.example.clientjetpack.Logs.Functions.normalizeToDay
-import com.example.clientjetpack.Logs.Functions.normalizeToWeekStart
+import java.util.Calendar
 
 class MapsIDSDatesHistoriqueTransactions {
     // Maps to store hierarchical relationship data
@@ -41,7 +40,8 @@ class MapsIDSDatesHistoriqueTransactions {
             dayMap.getOrPut(dayTimestamp) { mutableListOf() }.add(transaction.vid)
 
             // Add transaction to client map
-            clientMap.getOrPut(transaction.clientAcheteurID) { mutableListOf() }.add(transaction.vid)
+            clientMap.getOrPut(transaction.clientAcheteurID) { mutableListOf() }
+                .add(transaction.vid)
 
             // Store transaction state
             txStateMap[transaction.vid] = transaction.etateActuellementEst
@@ -56,7 +56,31 @@ class MapsIDSDatesHistoriqueTransactions {
         return this
     }
 
+    /**
+     * Normalizes a timestamp to midnight of the day (00:00:00.000)
+     */
+    fun normalizeToDay(timestamp: Long): Long {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = timestamp
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        return calendar.timeInMillis
+    }
 
+    /**
+     * Normalizes a timestamp to the first day of the week containing the timestamp
+     */
+    fun normalizeToWeekStart(dayTimestamp: Long): Long {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = dayTimestamp
+            // Set to first day of week (usually Sunday or Monday depending on locale)
+            set(Calendar.DAY_OF_WEEK, firstDayOfWeek)
+        }
+        return calendar.timeInMillis
+    }
 
 
 }
