5db587c6b8b14c4fa66a8331dd85ed99a22eccae
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-11 09:48:13 +0100

    .

--- app/src/androidTest/java/com/example/clientjetpack/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/clientjetpack/ExampleInstrumentedTest.kt
@@ -1,15 +1,11 @@
 package com.example.clientjetpack
 
-import V.DiviseParSections.App.SectionId7.PresentoirApplication.App.FragId1.Prix.Fragment.DataBase.Models.OutputNoSqlModel
-import V.DiviseParSections.App.SectionId7.PresentoirApplication.App.FragId1.Prix.Fragment.Log.logProduits
-import V.DiviseParSections.App.SectionId7.PresentoirApplication.App.FragId1.Prix.Fragment.Passive.strDateEtTempFromVidTimestamp
 import V.DiviseParSections.App.SectionId7.PresentoirApplication.App.FragId1.Prix.Fragment.ViewModel.TarificationViewModel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -17,7 +13,6 @@ import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,13 +21,21 @@ import org.junit.runner.RunWith
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class InstrumentedTest : KoinComponent {
 
     @get:Rule
     val rule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val logFilterRule = EnhancedLogFilterRule(
+        includeOnly = listOf("TestRunner", "System.out"),
+        excludeTags = listOf(
+            "ConfigStore", "ANDR-PERF-MPCTL", "chatty", "NetworkSession",
+            "vendor.qti.hardware", "ogle.android.g"
+        )
+    )
 
     private val testDispatcher = StandardTestDispatcher()
 
@@ -58,8 +61,6 @@ class InstrumentedTest : KoinComponent {
             1L,
             viewModel.getSqlClient(1)?.idActiveTypeTarificationDataBase
         )
-
-        SepareReferentialDataBases()
     }
 
     @Test
@@ -70,48 +71,5 @@ class InstrumentedTest : KoinComponent {
             2L,
             viewModel.getSqlClient(1)?.idActiveTypeTarificationDataBase
         )
-
-        val name = "A_DataBasesSepareReferential_AfterUpdate"
-
-        val currentStrTime = strDateEtTempFromVidTimestamp(System.currentTimeMillis())
-        println("\n========Apre Update========\n")
-        println(
-            "======== C Le Test Log Output Print Du Temp=${currentStrTime.first} " +
-                    "${currentStrTime.second} du  $name  ========"
-        )
-
-        testDispatcher.scheduler.advanceUntilIdle()
-
-        val currentValue = viewModel.outputNoSqlFlow.first()
-
-        mainLog(currentValue)
-
-        println("\n========TEST $name COMPLETED SUCCESSFULLY ========\n")
-    }
-
-    private fun SepareReferentialDataBases() = runTest {
-        try {
-            val name = "A_DataBasesSepareReferential"
-            val currentStrTime = strDateEtTempFromVidTimestamp(System.currentTimeMillis())
-            println(
-                "======== C Le Test Log Output Print Du Temp=${currentStrTime.first} " +
-                        "${currentStrTime.second} du  $name  ========"
-            )
-
-            testDispatcher.scheduler.advanceUntilIdle()
-            val currentValue = viewModel.outputNoSqlFlow.first()
-
-            mainLog(currentValue)
-
-            println("\n========TEST $name COMPLETED SUCCESSFULLY ========\n")
-
-        } catch (e: Exception) {
-            assertTrue("Exception during filtering: ${e.message}", false)
-        }
-    }
-
-    private fun mainLog(value: OutputNoSqlModel) {
-        println("\n-- Hierarchical Structure --")
-        logProduits(value, viewModel)
     }
 }
