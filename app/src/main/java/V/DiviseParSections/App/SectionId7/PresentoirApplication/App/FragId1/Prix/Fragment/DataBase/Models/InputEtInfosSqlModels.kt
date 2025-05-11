21b8ed0e89d324d6576e79a609759f9847489fc5
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-11 14:59:05 +0100

    .

--- app/src/main/java/V/DiviseParSections/App/SectionId7/PresentoirApplication/App/FragId1/Prix/Fragment/DataBase/Models/InputEtInfosSqlModels.kt
+++ app/src/main/java/V/DiviseParSections/App/SectionId7/PresentoirApplication/App/FragId1/Prix/Fragment/DataBase/Models/InputEtInfosSqlModels.kt
@@ -3,32 +3,27 @@ package V.DiviseParSections.App.SectionId7.PresentoirApplication.App.FragId1.Pri
 import androidx.room.PrimaryKey
 
 class InputEtInfosSqlModels {
-
     data class ProduitInfos(
         @PrimaryKey(autoGenerate = true)
         val id: Long = 0,
         val nom: String,
     )
-
     data class ClientDataBase(
         @PrimaryKey(autoGenerate = true)
         val id: Long = 0,
         val nom: String = "Non Difinie",
         val idActiveTypeTarificationDataBase: Long = 0,
     )
-
     data class TypeTarificationDataBase(
         @PrimaryKey(autoGenerate = true)
         val id: Long = 0,
         val typeTarificationEnum: TypeTarificationEnum,
     )
-
     enum class TypeTarificationEnum {
         ParBenifice,
         Historique,
         LeMaxPrixArrive
     }
-
     data class Tarification(
         val vidTimestamp: Long,
         val idProduit: Long,
