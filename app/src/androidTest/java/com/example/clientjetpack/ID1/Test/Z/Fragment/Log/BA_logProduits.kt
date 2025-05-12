496723509195a833b90c27b977aa796c01a3f47b
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-12 20:48:46 +0100

    .

--- app/src/androidTest/java/com/example/clientjetpack/ID1/Test/Z/Fragment/Log/BA_logProduits.kt
+++ app/src/androidTest/java/com/example/clientjetpack/ID1/Test/Z/Fragment/Log/BA_logProduits.kt
@@ -1,28 +1,31 @@
 package com.example.clientjetpack.ID1.Test.Z.Fragment.Log
 
-import com.example.clientjetpack.ID1.Test.Z.Fragment.Passive.strDateEtTempFromVidTimestamp
 import com.example.clientjetpack.ID1.Test.Z.Fragment.A.ViewModel.TarificationViewModel
 import com.example.clientjetpack.ID1.Test.Z.Fragment.DataBase.Models.OutputNoSqlModel
+import com.example.clientjetpack.ID1.Test.Z.Fragment.Passive.strDateEtTempFromVidTimestamp
 
-fun logProduits(value: OutputNoSqlModel, viewModel: TarificationViewModel) {
-        value.produits.forEachIndexed { produitIndex, produit ->
-            val isLastProduit = produitIndex == value.produits.size - 1
-            val produitPrefix = TreePrefix.Type1.get(isLastProduit)
+fun logProduits(
+    value: OutputNoSqlModel,
+    viewModel: TarificationViewModel,
+) {
+    value.produits.forEachIndexed { produitIndex, produit ->
+        val isLastProduit = produitIndex == value.produits.size - 1
+        val produitPrefix = TreePrefix.Type1.get(isLastProduit)
 
-            val (produitDate, produitTime) = strDateEtTempFromVidTimestamp(
-                produit.vidTimestamp
-            )
-            val relatedInfos = viewModel.getSqlProduitInfos(produit.id)
+        val (produitDate, produitTime) = strDateEtTempFromVidTimestamp(
+            produit.vidTimestamp
+        )
+        val relatedInfos = viewModel.getSqlProduitInfos(produit.id)
 
-            val produitInfos = StringBuilder().apply {
-                append(produitPrefix)
-                append(" Product : ")
-                append(produit.id)
-                append("=(${relatedInfos?.nom ?: " Unknown "})")
-            }.toString()
+        val produitInfos = StringBuilder().apply {
+            append(produitPrefix)
+            append(" Product : ")
+            append(produit.id)
+            append("=(${relatedInfos?.nom ?: " Unknown "})")
+        }.toString()
 
-            println("$produitInfos, Date: $produitDate Time: $produitTime (${produit.clients.size} clients)")
+        println("$produitInfos, Date: $produitDate Time: $produitTime (${produit.clients.size} clients)")
 
-            logClients(viewModel,produit.clients, isLastProduit)
-        }
+        logClients(viewModel, produit.clients, isLastProduit)
     }
+}
