8a7ea64548eba2491b868a26b66ef2152f9a9bc3
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-14 17:32:39 +0100

    Revert "."
    This reverts commit af29aff4

--- app/src/main/java/V/DiviseParSections/App/SectionId7/PresentoirApplication/App/Test/Produit.kt
+++ app/src/main/java/V/DiviseParSections/App/SectionId7/PresentoirApplication/App/Test/Produit.kt
@@ -1,11 +1,5 @@
 package V.DiviseParSections.App.SectionId7.PresentoirApplication.App.Test
 
-import io.realm.kotlin.ext.realmListOf
-import io.realm.kotlin.types.RealmList
-import io.realm.kotlin.types.RealmObject
-import io.realm.kotlin.types.annotations.PrimaryKey
-import org.mongodb.kbson.ObjectId
-
 data class Produit(
     val id: Long = 0,
     val timestamp: Long = System.currentTimeMillis(),
@@ -64,34 +58,6 @@ data class Produit(
                 val timestamp: Long = System.currentTimeMillis(),
                 val valeur: Double = 0.0,
             )
-        }
-    }
-}
-
-class RealmDataBase {
-    var childsLists: RealmList<Produit> = realmListOf()
-
-    class Produit : RealmObject {
-        @PrimaryKey
-        var bsonObjectId: ObjectId = ObjectId()
-        var infosKey: String = "nom"
-        var childsList: RealmList<Client> = realmListOf()
-
-        class Client : RealmObject {
-            @PrimaryKey
-            var bsonObjectId: ObjectId = ObjectId()
-            var childsList: RealmList<TypeTarification> = realmListOf()
-
-            class TypeTarification : RealmObject {
-                @PrimaryKey
-                var bsonObjectId: ObjectId = ObjectId()
-                var childsList: RealmList<Tarification> = realmListOf()
-
-                class Tarification : RealmObject {
-                    @PrimaryKey
-                    var bsonObjectId: ObjectId = ObjectId()
-                }
-            }
         }
     }
 }
