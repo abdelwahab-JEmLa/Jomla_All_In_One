7eca6fad7dc9d3b63028b5699f404387aaa71fdd
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-17 16:34:50 +0100

    Revert "."
    This reverts commit af29aff4

---
+++ app/src/main/java/V/DiviseParSections/App/SectionId7/PresentoirApplication/App/FragId1/Prix/Fragment/View/PreviewTest.kt
@@ -0,0 +1,13 @@
+package V.DiviseParSections.App.SectionId7.PresentoirApplication.App.FragId1.Prix.Fragment.View
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun PreviewTest() {
+//    FragmentMain(
+//        selectedProductId = 1,
+//        selectedClientId = 1
+//    )
+}
