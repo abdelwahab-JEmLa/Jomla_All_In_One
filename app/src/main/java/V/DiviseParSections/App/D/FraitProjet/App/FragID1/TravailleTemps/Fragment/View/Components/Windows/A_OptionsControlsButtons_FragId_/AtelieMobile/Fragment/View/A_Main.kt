ee195c8205998a65f1e9f5e90aa61b2aa4376976
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-19 06:17:09 +0100

    Revert "."
    This reverts commit af29aff4

--- app/src/main/java/V/DiviseParSections/App/D/FraitProjet/App/FragID1/TravailleTemps/Fragment/View/Components/Windows/A_OptionsControlsButtons_FragId_/AtelieMobile/Fragment/View/A_Main.kt
+++ app/src/main/java/V/DiviseParSections/App/D/FraitProjet/App/FragID1/TravailleTemps/Fragment/View/Components/Windows/A_OptionsControlsButtons_FragId_/AtelieMobile/Fragment/View/A_Main.kt
@@ -30,6 +30,7 @@ import org.koin.androidx.compose.koinViewModel
 fun FragmentMain(
     viewModel: TarificationViewModel = koinViewModel(),
 ) {
+
     val uiState by viewModel.uiState
     val clientId = viewModel.ancienRepoOuvertClientId
     val selectedProductId = uiState.produitAncienDB!!.idArticle.toLong()
