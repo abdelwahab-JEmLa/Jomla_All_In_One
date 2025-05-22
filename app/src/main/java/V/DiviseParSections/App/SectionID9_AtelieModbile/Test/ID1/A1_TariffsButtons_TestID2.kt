a571dfdbae604d651ba6cad28b9119ac1943be4d
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-22 12:30:58 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/SectionID9_AtelieModbile/Test/ID1/A1_TariffsButtons_TestID2.kt
+++ app/src/main/java/V/DiviseParSections/App/SectionID9_AtelieModbile/Test/ID1/A1_TariffsButtons_TestID2.kt
@@ -32,7 +32,6 @@ fun TariffsButtons_TestID2(
     filterBonId: Long = 1,
     fermeDialog: () -> Unit,
 ) {
-
     var afficheButtons by remember { mutableStateOf(false) }
     val uiState by viewModel.uiState.collectAsState()
     val tarificationList = testD_TarificationInfosT2()
