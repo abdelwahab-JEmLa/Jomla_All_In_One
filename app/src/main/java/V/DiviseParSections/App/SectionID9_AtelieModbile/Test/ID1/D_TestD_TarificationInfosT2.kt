d0a9bda0d051c034d47e2bcc241e03bd8e5a9d3a
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-05-23 10:07:23 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/SectionID9_AtelieModbile/Test/ID1/D_TestD_TarificationInfosT2.kt
+++ app/src/main/java/V/DiviseParSections/App/SectionID9_AtelieModbile/Test/ID1/D_TestD_TarificationInfosT2.kt
@@ -2,9 +2,7 @@ package V.DiviseParSections.App.SectionID9_AtelieModbile.Test.ID1
 
 fun testD_TarificationInfosT2(): List<D_TarificationInfos> {
     val idProduit1: Long = 849
-    val idProduit2: Long = 859
     val parentIdClient: Long = 4
-    val parentIdClient2: Long = 3
 
     return listOf(
         D_TarificationInfos(
