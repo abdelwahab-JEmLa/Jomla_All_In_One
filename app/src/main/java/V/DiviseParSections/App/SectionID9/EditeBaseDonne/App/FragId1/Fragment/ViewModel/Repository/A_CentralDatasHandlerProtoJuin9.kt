2117cdfa047f60cf1b25a34e807e3c34b0131a36
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-06-17 18:51:03 +0100

    before Merg

--- app/src/main/java/V/DiviseParSections/App/SectionID9/EditeBaseDonne/App/FragId1/Fragment/ViewModel/Repository/A_CentralDatasHandlerProtoJuin9.kt
+++ app/src/main/java/V/DiviseParSections/App/SectionID9/EditeBaseDonne/App/FragId1/Fragment/ViewModel/Repository/A_CentralDatasHandlerProtoJuin9.kt
@@ -1,18 +1,17 @@
 package V.DiviseParSections.App.SectionID9.EditeBaseDonne.App.FragId1.Fragment.ViewModel.Repository
 
 import V.DiviseParSections.App.SectionID9.EditeBaseDonne.App.FragId1.Fragment.ViewModel.Repository.A1.Proto.Juin17.Proto.D_AchatOperation.Repository.D_AchatOperationComposeRepositoryPJ17
+import V.DiviseParSections.App.SectionID9.EditeBaseDonne.App.FragId1.Fragment.ViewModel.Repository.A1.Proto.Juin17.Proto.Z_AppCompt.Repository.Z_AppComptComposeRepositoryProtoJuin17
 import V.DiviseParSections.App.SectionID9.EditeBaseDonne.App.FragId1.Fragment.ViewModel.Repository.A1.Proto.Juin17.Proto.Z_DatabaseInitializationManager
 import V.DiviseParSections.App.SectionID9.EditeBaseDonne.App.FragId1.Fragment.ViewModel.Repository.A2_Passive.B_ClientsState
 import V.DiviseParSections.App.SectionID9.EditeBaseDonne.App.FragId1.Fragment.ViewModel.Repository.A2_Passive.C_TransactionCommercialState
 import Z_CodePartageEntreApps.DataBase.Juin3.Proto.A_MasterRepositorysGrpProtoJuin3
-import Z_CodePartageEntreApps.DataBase.Z_AppComptRepository.Base.Juin17.Proto.Z_AppComptRepositoryProtoJuin17
 import Z_CodePartageEntreApps.Repository._1_3_TransactionCommercial.C3_TransactionCommercial
 import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -22,17 +21,20 @@ class A_CentralDatasHandlerProtoJuin9(
     private val context: Context,
     val databaseInitializationManager: Z_DatabaseInitializationManager,
     val comptAppState: D_ComptAppState,
-    val appComptComposeRepository: Z_AppComptRepositoryProtoJuin17,
+    val appComptComposeRepositoryProtoJuin17: Z_AppComptComposeRepositoryProtoJuin17,
 
     val a_MasterRepositorysGrpProtoJuin3: A_MasterRepositorysGrpProtoJuin3,
-    val d_AchatOperationComposeRepositoryPJ17: D_AchatOperationComposeRepositoryPJ17,
     val b3CategoriesCompoRepository: B3CategoriesCompoRepository,
     val clientsState: B_ClientsState,
     val transactionCommercialState: C_TransactionCommercialState,
+
+    val d_AchatOperationComposeRepositoryPJ17: D_AchatOperationComposeRepositoryPJ17,
 ) {
     private val composScope = CoroutineScope(Dispatchers.IO)
-    private val _loadingProgress = mutableFloatStateOf(0f)
-    val loadingProgress: Float? by derivedStateOf { _loadingProgress.floatValue }
+
+    val loadingProgress: Float? by derivedStateOf {
+        appComptComposeRepositoryProtoJuin17.currentAppCompt?.mainInitDataBaseProgressEtate
+    }
 
     val nombreClientsOuLeurDernierEtateCible: Int by derivedStateOf {
         clientsState.datasValue.count { client ->
@@ -42,6 +44,7 @@ class A_CentralDatasHandlerProtoJuin9(
             )
         }
     }
+
     val clientOuSonMarqueMapEstOuvert by derivedStateOf {
         clientsState.findClientById(
             comptAppState.idClientOuSonMarqueMapEstOuvert
@@ -70,14 +73,6 @@ class A_CentralDatasHandlerProtoJuin9(
                 databaseInitializationManager.initializeAllRepositories(context)
             } catch (e: Exception) {
                 databaseInitializationManager.updateMainInitDataBaseProgressEtate(1.0f)
-            }
-        }
-
-        composScope.launch {
-            a_MasterRepositorysGrpProtoJuin3.model.collect { masterModel ->
-                masterModel?.let { model ->
-                    _loadingProgress.floatValue = model.progress
-                }
             }
         }
     }
