1075437a5e6b486549f1c04e12514b5e8b7986d1
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-06-26 18:55:02 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/B/ClientUisView/App/FragID2/PanierFinaleDAchat/Package/Views/B_MainList/Z/Z/View/Z/Main/PanierFinaleDAchatSec1Frag3.kt
+++ app/src/main/java/V/DiviseParSections/App/B/ClientUisView/App/FragID2/PanierFinaleDAchat/Package/Views/B_MainList/Z/Z/View/Z/Main/PanierFinaleDAchatSec1Frag3.kt
@@ -2,6 +2,7 @@ package V.DiviseParSections.App.B.ClientUisView.App.FragID2.PanierFinaleDAchat.P
 
 import V.DiviseParSections.App.B.ClientUisView.App.FragID2.PanierFinaleDAchat.Package.Views.B_MainList.Z.A.ViewModel.ZViewModel_Sec1Frag3
 import V.DiviseParSections.App.B.ClientUisView.App.FragID2.PanierFinaleDAchat.Package.Views.B_MainList.Z.Z.View.ListAchats.View.A.List.MainList
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import org.koin.androidx.compose.koinViewModel
@@ -11,13 +12,26 @@ fun PanierFinaleDAchatSec1Frag3(
     modifier: Modifier = Modifier,
     viewModel: ZViewModel_Sec1Frag3 = koinViewModel()
 ) {
-    val fCouleurAchatOperationRepositoryComposable = viewModel.uiStateCentralRepositorys.fCouleurAchatOperationRepositoryComposable
+    val fCouleurAchatOperationRepositoryComposable =
+        viewModel.uiStateCentralRepositorys.fCouleurAchatOperationRepositoryComposable
 
-    MainList(
-        modifier = modifier,
-        viewModel = viewModel,
-        fCouleurAchatOperationRepositoryComposable = fCouleurAchatOperationRepositoryComposable
-    )
+    Column {
+        DetailsBonVent(viewModel = viewModel)
+
+        MainList(
+            modifier = modifier,
+            viewModel = viewModel,
+            fCouleurAchatOperationRepositoryComposable = fCouleurAchatOperationRepositoryComposable
+        )
+    }
+}
+
+@Composable
+fun DetailsBonVent(
+    modifier: Modifier = Modifier,
+    viewModel: ZViewModel_Sec1Frag3
+) {
+
 }
 
 
