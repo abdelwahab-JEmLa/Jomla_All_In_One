03f286faac3eed3b534cc8a72bd5d88b165e5a13
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-06-29 08:35:25 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/SectionID12/GrossistAchat/App/FragID1/CommandeProduits/Fragment/Preview.kt
+++ app/src/main/java/V/DiviseParSections/App/SectionID12/GrossistAchat/App/FragID1/CommandeProduits/Fragment/Preview.kt
@@ -1,4 +1,4 @@
-package Z_CodePartageEntreApps.Proto.B.Par.App.A.AchatsManager.App._1.Shared.Module
+package V.DiviseParSections.App.SectionID12.GrossistAchat.App.FragID1.CommandeProduits.Fragment
 
 import V.DiviseParSections.App.SectionID12.GrossistAchat.App.FragID1.CommandeProduits.Fragment.View.A.Main.Screen_GrossistAchatSec12FragID1
 import androidx.compose.runtime.Composable
