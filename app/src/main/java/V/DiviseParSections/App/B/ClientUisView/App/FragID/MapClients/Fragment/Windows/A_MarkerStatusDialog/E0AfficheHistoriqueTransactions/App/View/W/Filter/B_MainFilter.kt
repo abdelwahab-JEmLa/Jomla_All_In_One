ff331cc3aeb2692e176cc4dfcb69f1cea1212394
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-06-29 23:29:49 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/B/ClientUisView/App/FragID/MapClients/Fragment/Windows/A_MarkerStatusDialog/E0AfficheHistoriqueTransactions/App/View/W/Filter/B_MainFilter.kt
+++ app/src/main/java/V/DiviseParSections/App/B/ClientUisView/App/FragID/MapClients/Fragment/Windows/A_MarkerStatusDialog/E0AfficheHistoriqueTransactions/App/View/W/Filter/B_MainFilter.kt
@@ -3,8 +3,6 @@ package V.DiviseParSections.App.B.ClientUisView.App.FragID.MapClients.Fragment.W
 import V.DiviseParSections.App.B.ClientUisView.App.FragID.MapClients.Fragment.Windows.A_MarkerStatusDialog.E0AfficheHistoriqueTransactions.App.View.Z.List.View_MainList
 import V.DiviseParSections.App.B.ClientUisView.App.FragID.MapClients.Fragment.Windows.A_MarkerStatusDialog.E0AfficheHistoriqueTransactions.App.ViewModel.E0AfficheHistoriqueTransactionsViewModel
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -20,12 +18,9 @@ fun MainFilter(
         datasGBonVentRepository
             .filter { it.parentHClientKeyID == uiState.comptOnAfficheHistoriquesClientKeyId }
             .sortedByDescending { it.creationTimestamps }
-    Column {
-        Text(datasGBonVentRepository.size.toString())
-        Text(listGBonVentFilteredByClientKeySorted.size.toString())
-        View_MainList(
-            viewModel,
-            listGBonVentFilteredByClientKeySorted,
-        )
-    }
+
+    View_MainList(
+        viewModel,
+        listGBonVentFilteredByClientKeySorted,
+    )
 }
