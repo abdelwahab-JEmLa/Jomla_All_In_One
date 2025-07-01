3e38b8d903b16ad4091dfc268735386a38671e7e
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-07-01 03:26:45 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/Shared/A/MemoireVive/Debug/ID1/Test/Filter/B_MainFilter.kt
+++ app/src/main/java/V/DiviseParSections/App/Shared/A/MemoireVive/Debug/ID1/Test/Filter/B_MainFilter.kt
@@ -3,23 +3,14 @@ package V.DiviseParSections.App.Shared.A.MemoireVive.Debug.ID1.Test.Filter
 import V.DiviseParSections.App.Shared.A.MemoireVive.Debug.ID1.Test.List.View_MainList
 import V.DiviseParSections.App.Shared.A.MemoireVive.Debug.ID1.Test.ViewModel.E0AfficheHistoriqueTransactionsViewModel
 import V.DiviseParSections.App.Shared.Repository.HClientInfos
-import V.DiviseParSections.App.Shared.Repository.Z_AppCompt
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.wear.compose.material.Text
 
 @Composable
 fun MainFilter(vm: E0AfficheHistoriqueTransactionsViewModel, tagParentKey_Client: String) {
-    val targetKey =  HClientInfos.extractSonKeyByParent(tagParentKey_Client)
-
     val filtered = vm.getter.gBonVentRepository.datasValue.filter {
-        HClientInfos.extractSonKeyByParent(it.keyByParent) == targetKey
+        HClientInfos.extractSonKeyByParent(it.keyByParent) == HClientInfos.extractSonKeyByParent(tagParentKey_Client)
     }.sortedByDescending { it.creationTimestamps }
 
-    Column {
-       Z_AppCompt.extractSonKeyByParent(tagParentKey_Client,)?.let { Text("ID7: $it", color = Red) }
-        View_MainList(vm, filtered, Modifier)
-    }
+    View_MainList(vm, filtered, Modifier)
 }
