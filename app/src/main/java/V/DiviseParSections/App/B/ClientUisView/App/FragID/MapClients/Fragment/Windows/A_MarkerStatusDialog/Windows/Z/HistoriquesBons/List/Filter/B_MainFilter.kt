12543ac5b0c60a806c7e4e1d6a3e04ecc4cc6e65
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-07-08 00:43:36 +0100

    save

--- app/src/main/java/V/DiviseParSections/App/B/ClientUisView/App/FragID/MapClients/Fragment/Windows/A_MarkerStatusDialog/Windows/Z/HistoriquesBons/List/Filter/B_MainFilter.kt
+++ app/src/main/java/V/DiviseParSections/App/B/ClientUisView/App/FragID/MapClients/Fragment/Windows/A_MarkerStatusDialog/Windows/Z/HistoriquesBons/List/Filter/B_MainFilter.kt
@@ -2,7 +2,9 @@ package V.DiviseParSections.App.B.ClientUisView.App.FragID.MapClients.Fragment.W
 
 import V.DiviseParSections.App.B.ClientUisView.App.FragID.MapClients.Fragment.Windows.A_MarkerStatusDialog.Windows.Z.HistoriquesBons.List.List.View_MainList
 import V.DiviseParSections.App.B.ClientUisView.App.FragID.MapClients.Fragment.Windows.A_MarkerStatusDialog.Windows.Z.HistoriquesBons.List.ViewModel.E0AfficheHistoriqueTransactionsViewModel
+import V.DiviseParSections.App.Shared.Repository.A.Base.A.Bsetter.Helper.DebugsTests.getSemanticsTag
 import V.DiviseParSections.App.Shared.Repository.ID2ClientRepository.Repository.HClientInfos
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -12,9 +14,12 @@ fun MainFilter(
     modifier: Modifier,
     vm: E0AfficheHistoriqueTransactionsViewModel,
 ) {
-    val filtered = vm.getter.repo8BonVent.datasValue.filter {
+    val datasValue = vm.getter.repo8BonVent.datasValue
+    val filtered = datasValue.filter {
         it.parentM2ClientInfosKey == (markerStatusDialogM2Client?.keyID ?: "")
     }.sortedByDescending { it.creationTimestamps }
 
-    View_MainList(vm, filtered, modifier)
+    ElevatedCard(Modifier.getSemanticsTag(datasValue, "datasValue")) {
+        View_MainList(filtered, vm)
+    }
 }
