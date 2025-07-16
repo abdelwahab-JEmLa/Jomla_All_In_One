3b4df370a6b26e5cebc16c2862f0e7d45338e134
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-07-16 17:30:19 +0100

    Save

--- app/src/main/java/V/DiviseParSections/App/Shared/Repository/Repo18P/Repository/ParametresAppComptNonSaved.kt
+++ app/src/main/java/V/DiviseParSections/App/Shared/Repository/Repo18P/Repository/ParametresAppComptNonSaved.kt
@@ -4,9 +4,9 @@ import V.DiviseParSections.App._0.Navigation.Screen
 
 data class ParametresAppComptNonSaved(
     val itsDevMode: Boolean = true,
-    val abdelwahabCompt: String = "-OV9dYujH9cA3yEx8AYT",
-    val abdelmoumen: String = "b1",
-    val currentActiveFocucedM9AppComptKeyID: String = "b1",
+    val abdelwahabCompt_KeyId: String = "-OV9dYujH9cA3yEx8AYT",
+    val abdelmomen_Compt_KeyId: String = "-OTmoNn0cljrRuhVR2s4",
+    val currentActiveFocucedM9AppComptKeyID: String = abdelmomen_Compt_KeyId,
     val currentActiveFocucedM9AppComptDebugInfos: String = "",
 
     val activeWindowsSearchProduit: Boolean = false,
