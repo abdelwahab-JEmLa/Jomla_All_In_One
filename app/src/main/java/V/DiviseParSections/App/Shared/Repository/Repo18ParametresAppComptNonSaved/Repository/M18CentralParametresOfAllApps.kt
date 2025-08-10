68c30450a86deef706dae0406d64dabb1d18c7c0
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-08-10 09:38:30 +0100

    Save

--- app/src/main/java/V/DiviseParSections/App/Shared/Repository/Repo18ParametresAppComptNonSaved/Repository/M18CentralParametresOfAllApps.kt
+++ app/src/main/java/V/DiviseParSections/App/Shared/Repository/Repo18ParametresAppComptNonSaved/Repository/M18CentralParametresOfAllApps.kt
@@ -91,7 +91,7 @@ data class M18CentralParametresOfAllApps(
     val abdelwahabCompt_KeyId_DPL: String = "-OV9edQZecDczbx-ndPl",
     val abdelmomen_Compt_KeyId: String = "-OTmoNn0cljrRuhVR2s4",
 
-    val au_Lence_Set_Compt_Ac_KeyId: String = abdelwahabCompt_KeyId,
+    val au_Lence_Set_Compt_Ac_KeyId: String = abdelmomen_Compt_KeyId,
 
     val activeWindowsSearchProduit: Boolean = false,
     var enablePerformAutoClickImageDisplayer: Boolean = false,
