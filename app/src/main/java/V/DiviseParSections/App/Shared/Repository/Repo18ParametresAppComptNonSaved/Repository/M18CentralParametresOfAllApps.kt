db61460b733e63c56028525a25c5447a311b19b4
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2025-09-16 22:09:26 +0100

    Save

--- app/src/main/java/V/DiviseParSections/App/Shared/Repository/Repo18ParametresAppComptNonSaved/Repository/M18CentralParametresOfAllApps.kt
+++ app/src/main/java/V/DiviseParSections/App/Shared/Repository/Repo18ParametresAppComptNonSaved/Repository/M18CentralParametresOfAllApps.kt
@@ -85,7 +85,7 @@ data class M18CentralParametresOfAllApps(
     val keyId: String = "M18CentralParametresOfAllApps",
 
     //---------------------------------Developing.Tools---------------------------------------------------------------------------------------------------------------------------------
-    val itsDevMode: Boolean = true ,
+    val itsDevMode: Boolean = false ,
     val devStartUpScree: String = Screen.Screen1PanieVentsFinale.route,
 
     val desactive_Animation_Pour_LayoutInspector: Boolean = false,
