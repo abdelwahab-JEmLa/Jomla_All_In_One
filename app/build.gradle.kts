655f69fc434b49c4fec87f76daeb7b0c8bf11835
abdelwahab-JEmLa <156733337+abdelwahab-JEmLa@users.noreply.github.com>
2026-02-13 00:16:24 +0100

    save

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.11.delivery.4"
+        versionName = "1.11.delivery.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
